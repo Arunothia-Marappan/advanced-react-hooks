48e033859d1002a22ecab58b7efb04db39481193
Arunothia <arunothiamarappan@gmail.com>
2024-03-10 17:46:02 -0600

    Instead of expecting the user of the custom hook to provide callback function, we can return a callback based promise to the user from the custom hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -27,7 +27,7 @@ function asyncReducer(state, action) {
   }
 }
 
-function useAsync(asyncCallback, initialState) {
+function useAsync(initialState) {
   const [state, dispatch] = React.useReducer(asyncReducer, {
     status: 'idle',
     data: null,
@@ -35,8 +35,7 @@ function useAsync(asyncCallback, initialState) {
     ...[initialState],
   })
 
-  React.useEffect(() => {
-    const promise = asyncCallback()
+  const run = React.useCallback(promise => {
     if (!promise) {
       return
     }
@@ -49,24 +48,23 @@ function useAsync(asyncCallback, initialState) {
         dispatch({type: 'rejected', error})
       },
     )
-  }, [asyncCallback])
+  }, [])
 
-  return state
+  return {...state, run}
 }
 
 function PokemonInfo({pokemonName}) {
-  const asyncCallback = React.useCallback(() => {
+  const {status, data, error, run} = useAsync({
+    status: pokemonName ? 'pending' : 'idle',
+  })
+
+  React.useEffect(() => {
     if (!pokemonName) {
       return
     }
-    return fetchPokemon(pokemonName)
-  }, [pokemonName])
-  const state = useAsync(
-    asyncCallback,
-    {satus: pokemonName ? 'pending' : 'idle'},
-    [pokemonName],
-  )
-  const {status, data, error} = state
+    const pokemonPromise = fetchPokemon(pokemonName)
+    run(pokemonPromise)
+  }, [pokemonName, run])
 
   switch (status) {
     case 'idle':
